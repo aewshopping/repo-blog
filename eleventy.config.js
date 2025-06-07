916e239b0fd6931601e8437db60d05eed907f349
aewshopping <aewshopping@gmail.com>
2025-06-07 18:12:14 +0100

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,14 @@
+import { DateTime } from "luxon";
+
 export default async function(eleventyConfig) {
 	// Configure Eleventy
 
     eleventyConfig.setInputDirectory("base");
 
     eleventyConfig.setOutputDirectory("");
+	
+    eleventyConfig.addFilter("myDate", dateObj => {
+      return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("d LLLL yyyy");
+    });
+
 };
